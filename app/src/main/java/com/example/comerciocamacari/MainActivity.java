b28e4b4d439df93a1a51b28abcb418c0ccb7f112
bl4kwid0w <83369787+bl4kwid0w@users.noreply.github.com>
2021-05-12 09:43:10 -0300

    icone do app

--- app/src/main/java/com/example/comerciocamacari/MainActivity.java
+++ app/src/main/java/com/example/comerciocamacari/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.comerciocamacari;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -10,12 +11,13 @@ import android.webkit.WebViewClient;
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        webView = (WebView) findViewById(R.id.webview);
+        webView = findViewById(R.id.webview);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("https://comerciocamacari.github.io");
 
